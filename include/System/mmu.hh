f31a20920408732b4849000a4267bc2eb8afab84
shihao-song <shihao.song@drexel.edu>
2019-07-19 21:15:08 -0400

    Keep working on MMU

--- include/System/mmu.hh
+++ include/System/mmu.hh
@@ -47,11 +47,31 @@ class TrainedMMU : public MMU
 class MFUToNearRows : public TrainedMMU
 {
   protected:
+    unsigned blk_size;
+
+    unsigned num_of_channels;
+    unsigned num_of_ranks;
+    unsigned num_of_banks;
+    unsigned num_of_partitions;
+
+    unsigned num_of_rows_per_partition;
+    unsigned num_of_cache_lines_per_row;
 
   public:
     MFUToNearRows(int num_of_cores, Config &cfg)
-        : TrainedMMU(num_of_cores, cfg)
+        : TrainedMMU(num_of_cores, cfg),
+          blk_size(cfg.block_size),
+          num_of_channels(cfg.num_of_channels),
+          num_of_ranks(cfg.num_of_ranks),
+          num_of_banks(cfg.num_of_banks),
+          num_of_partitions(cfg.num_of_parts)
     {}
+
+    // Limitations,
+    // (1) I assume a page is mapped across 8 banks, 4 ranks and 2 channels, which gives
+    // us the page size of 64 * 8 * 4 * 2 = 4 kB;
+    // (2) I assume there are 4 channels in total;
+    
 };
 }
 
