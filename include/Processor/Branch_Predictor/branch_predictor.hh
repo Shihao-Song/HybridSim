56990e17aaaa70261ea0cc9f8ca1c2ea0c7ba1d5
shihao-song <shihao.song@drexel.edu>
2020-01-26 08:24:25 -0500

    Keep working on branch predictor framework

--- include/Processor/Branch_Predictor/branch_predictor.hh
+++ include/Processor/Branch_Predictor/branch_predictor.hh
@@ -1,6 +1,18 @@
 #ifndef __BRANCH_PREDICTOR_HH__
 #define __BRANCH_PREDICTOR_HH__
 
+#include "Sim/instruction.hh"
 
+class Branch_Predictor
+{
+  public:
+    Branch_Predictor(unsigned _num_threads)
+        : num_threads(_num_threads)
+    {}
+
+  protected:
+    unsigned num_threads; // Some branch predictors need to maintain information
+                          // for each thread.
+};
 
 #endif
